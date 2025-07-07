2d5082ec8735598c8d7e30db1547d709172c79ef
WindyDante <125888129+WindyDante@users.noreply.github.com>
2025-07-07 21:55:36 +0800

    feat：添加UploadFile结构体以支持文件上传功能

--- internal/model/share/share.go
+++ internal/model/share/share.go
@@ -1,5 +1,7 @@
 package share
 
+import "mime/multipart"
+
 type Share struct {
 	ID         string `json:"id" gorm:"primaryKey"`
 	Content    string `json:"url" gorm:"unique;not null"` // URL是一个文件路径或文本内容
@@ -7,4 +9,10 @@ type Share struct {
 	ExpireUnit int64  `json:"expire_unit"` // 过期单位，秒、分钟、小时等
 	Status     int    `json:"status"`      // 状态，0表示未使用，1表示已使用，2表示已过期
 	Code       string `json:"code"`        // 访问码
+}
+
+type UploadFile struct {
+	File       *multipart.FileHeader `form:"file" `       // 非必须,文件
+	ExpireTime string                `form:"expireTime" ` // 无时间表示长期有效
+	Text       string                `form:"text"`        // 文本内容
 }
